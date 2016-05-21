8bc2f00884f8965ff51acaa324360fcc0e63643e
farid <farid@info74.ru>
2016-05-21 14:16:40 +0500

    Initial Commit

---
+++ iTunesSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTunesSearch
+//
+//  Created by Farid on 21.05.16.
+//  Copyright © 2016 Farid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iTunesSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTunesSearch
+//
+//  Created by Farid on 21.05.16.
+//  Copyright © 2016 Farid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
